2c5657da65f7896bcd1ce1104738b6a6eafb285f
jorbay <8785583+Jorbay@users.noreply.github.com>
2020-09-09 11:57:34 -0700

    Implement transformerEncoder using encoderLayer

--- algorithms/transformer/transformer.py
+++ algorithms/transformer/transformer.py
@@ -2,7 +2,9 @@
 # Standard PyTorch imports
 import torch
 import torch.nn as nn
-import math
+import math, copy
+from torch.nn.modules.container import ModuleList
+from torch import Tensor
 
 #From torch documentation for api:
 #https://pytorch.org/docs/stable/_modules/torch/nn/modules/transformer.html#TransformerEncoderLayer.forward
@@ -38,6 +40,30 @@ class Transformer():
         def __addAndNorm(self, ultima, penultima):
             sum = ultima + penultima
             return self.normalizer(sum)
+
+    class TransformerEncoder(nn.Module):
+        """
+        This is my  version of Pytorch's nn.TransformerEncoder, which takes
+        as argument an instance of an encoder_layer. The different layers of the
+        encoder are then created with a copy operation. I'm going to use the same
+        copy operation that pytorch uses for simplicity.
+        """
+
+        def __init__(self, encoder_layer, num_layers, norm=None):
+            super(Transformer.TransformerEncoder, self).__init__()
+            self.layers = self._get_clones(encoder_layer, num_layers)
+            self.num_layers = num_layers
+
+        def forward(self, src: Tensor, mask: Optional[Tensor] = None,
+                    src_key_padding_mask: Optional[Tensor] = None) -> Tensor:
+            output = src
+            for layer in self.layers:
+                output = layer.forward(output)
+            return output
+
+        # From Pytorch 1.6.0
+        def _get_clones(self, module, N):
+            return ModuleList([copy.deepcopy(module) for i in range(N)])
 
 
 class SingleLayerPerceptron(nn.Module):
