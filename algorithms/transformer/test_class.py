a4fc94b90e5354b7949d4f97f84253bf46ec5c61
jorbay <8785583+Jorbay@users.noreply.github.com>
2020-09-14 14:33:34 -0700

    Add assert for expected result from test_scaledDotProductAttention

--- algorithms/transformer/test_class.py
+++ algorithms/transformer/test_class.py
@@ -6,25 +6,25 @@ import torch
 class TestClass:
 
     def test_scaledDotProductAttention(self):
-        embeds = Embedding(512, 32000)
         d_model = 512
         d_k = 64
         number_of_heads = int(d_model / d_k)
-        number_of_tokens = 1
+        number_of_tokens = 3
 
-        example_word = torch.tensor([int(1)], dtype=torch.long)
-        example_embedded= embeds(example_word)
-
-        slp = SingleLayerPerceptron(d_model, d_k)
-        example_key = slp.forward(example_embedded)
-        example_value = slp.forward(example_embedded)
-        example_query = slp.forward(example_embedded)
-
+        example_query = torch.tensor([[1, 1], [1, 1], [1,1]], dtype=torch.float64)
+        example_key = example_query.detach().clone()
+        example_value = example_query.detach().clone()
         mask = make_mask(number_of_tokens)
-        print("Mask size is: ")
-        print(mask.size())
 
         multi_head_attention = MultiHeadAttention(number_of_heads, d_model)
 
-        multi_head_attention.scaledDotProductAttention(example_query, example_key, example_value)
-        multi_head_attention.scaledDotProductAttention(example_query, example_key, example_value, mask)
+        result_without_mask = multi_head_attention.scaledDotProductAttention(example_query, example_key, example_value)
+        result_with_mask = multi_head_attention.scaledDotProductAttention(example_query, example_key, example_value,
+                                                                          mask)
+        expected_result_without_mask = example_query.detach().clone()
+        expected_result_with_mask = torch.tensor([[1/3, 1/3], [5/6, 5/6], [11/6, 11/6]], dtype=torch.float64)
+
+        assert torch.equal(result_without_mask, expected_result_without_mask)
+
+        #TODO: Fix this assert for the expected result of mask.
+        #assert torch.equal(result_with_mask, expected_result_with_mask)
