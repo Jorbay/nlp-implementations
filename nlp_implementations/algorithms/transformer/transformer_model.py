e03efbbcedfb595ddadcdf6d482126e7204a61d1
jorbay <8785583+Jorbay@users.noreply.github.com>
2020-09-22 11:46:45 -0700

    Make first implementation of transformer_model

--- nlp_implementations/algorithms/transformer/transformer_model.py
+++ nlp_implementations/algorithms/transformer/transformer_model.py
@@ -5,4 +5,30 @@ from .transformer_modules import TransformerDecoder, TransformerEncoder, Transfo
 
 
 class TransformerModel(nn.Module):
-    pass
+
+    def __init__(self, ntokens_per_batch, nencoder_layers, ndecoder_layers, d_model, nheads, vocab):
+        self.ntokens_per_batch = ntokens_per_batch
+        self.nencoder_layers = nencoder_layers
+        self.ndecoder_layers = ndecoder_layers
+        self.d_model = d_model
+        self.nheads = nheads
+        self.vocab = vocab
+
+        self.d_k = int(self.d_model/ self.nheads)
+        self.token_encoder = TokenEncoder(self.d_model, self.vocab)
+        self.transformer_encoder_layer = TransformerEncoderLayer(self.d_mdoel, self.nheads)
+        self.transformer_encoder = TransformerEncoder(self.transformer_encoder, self.nencoder_layers)
+        self.transformer_decoder_layer = TransformerDecoderLayer(self.d_model, self.nheads)
+        self.transformer_decoder = TransformerDecoder(self.transformer_decoder_layer, self.ndecoder_layers)
+        self.token_decoder = TokenDecoder(self.d_model, self.vocab)
+
+    def forward(self, src):
+        src_encoded = self.token_encoder.forward(src)
+
+        #TODO: I need to also make an encoding of expected output (src shifted)
+        target = None
+
+        memory = self.transformer_encoder.forward(src)
+        output_encoded = self.transformer_decoder.forward(target, memory)
+
+        output = self.token_decoder.forward(output_encoded)
